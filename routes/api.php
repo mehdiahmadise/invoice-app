133d440e42581a6e0c67ba8b32b1314023b35139
Mehdi Ahmadi <mehdiahmadise@gmail.com>
2024-06-02 21:51:38 +0430

    invoice search and invoice list completed

---
+++ routes/api.php
@@ -0,0 +1,12 @@
+<?php
+
+use App\Http\Controllers\InvoiceController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/get_all_invoice', [InvoiceController::class, 'get_all_invoice']);
+Route::get('/search_invoice', [InvoiceController::class, 'search_invoice']);
--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
 import vue from "@vitejs/plugin-vue";
 
 export default defineConfig({
-    plugins: [vue()],
-    resolve: {
-        alias: {
-            "@": "/resources/js",
-        },
-    },
+    plugins: [
+        laravel({
+            input: ["resources/css/app.css", "resources/js/app.js"],
+            refresh: true,
+        }),
+        vue(),
+    ],
 });
