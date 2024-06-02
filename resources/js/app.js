81827b298d3a774aa06a70709e9895af24e53841
Mehdi Ahmadi <mehdiahmadise@gmail.com>
2024-06-02 14:23:58 +0430

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,7 @@
+import "./bootstrap";
+import { createApp } from "vue";
+import App from "./components/app.vue";
+
+import router from "./router/index";
+
+createApp(App).use(router).mount("#app");
---
+++ resources/js/components/NotFound.vue
@@ -0,0 +1,5 @@
+<template>
+    <h1>Not Found</h1>
+    <br />
+    <router-link to="/">Back To home page</router-link>
+</template>
